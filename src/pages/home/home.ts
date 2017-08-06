f26f24c5067e9f005224cdb38fe8b392f5a3183e
BasilJohn <ferdenend@gmail.com>
2017-08-06 08:42:17 +0530

    Get Data from api

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,49 +1,62 @@
 import { Component ,ViewChild, ElementRef, OnInit } from '@angular/core';
 import { NavController } from 'ionic-angular';
-import { MainServiceProvider } from '../../providers/main-service/main-service'
+import { MainServiceProvider } from '../../providers/main-service/main-service';
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html',
   providers: [MainServiceProvider]
 })
-export class HomePage  {
 
+
+export class HomePage  {
 
   public webdata: any;
   public cinemasAnchorList: any;
+  public htmlTemplate: any;
   constructor(public navCtrl: NavController,public mainService:MainServiceProvider) {
 
     this.loadWebData();
   }
 
-  //@ViewChild(this.webdata) inputData: ElementRef;
+  @ViewChild("qbc-div-cinema",{read: ElementRef}) inputData: ElementRef;
 
-  
-   getRequiredData()
+     getRequiredData(inputData)
   {
-// var divCinemasList = document.createElement('divCinemasList');
-//             divCinemasList.innerHTML = inputData;
-//             var cinemasdiv = divCinemasList.querySelector('#qbc-div-cinema')
-//             this.cinemasAnchorList = [];
-//            let element = inputData.nativeElement
-//             for (var i = 0; i < (cinemasdiv).find('a').length; i++) {
-//                this.cinemasAnchorList.push({
-//                     value: $(cinemasdiv).find('a')[i].getAttribute('onclick').replace('getAllCinemaData', '').replace(/[()]/g, '').split(',')[0].replace(/["']/g, ""),
-//                     name: $(cinemasdiv).find('a')[i].getAttribute('onclick').replace('getAllCinemaData', '').replace(/[()]/g, '').split(',')[1].replace(/["']/g, "")
-//                 });
-//             }
+          var divCinemasList = document.createElement('divCinemasList');
+          divCinemasList.innerHTML = inputData;
+           
+          var cinemasdiv = divCinemasList.querySelector('#qbc-div-cinema')
+          this.cinemasAnchorList = [];
+          let element = cinemasdiv.querySelectorAll('a');
+          //alert(element.length);
+          for (var i = 0; i < element.length; i++) {
+               this.cinemasAnchorList.push({
+                   value: element[i].getAttribute('onclick').replace('getAllCinemaData', '').replace(/[()]/g, '').split(',')[0].replace(/["']/g, ""),
+                    name: element[i].getAttribute('onclick').replace('getAllCinemaData', '').replace(/[()]/g, '').split(',')[1].replace(/["']/g, "")
+               });
+           }
+
+           for (var i = 0; i < this.cinemasAnchorList.length; i++) {
+             
+            alert(this.cinemasAnchorList[i].value);
+            alert(this.cinemasAnchorList[i].name);
+           }
+            //for (var i = 0; i < cinemasdiv; i++) {
+               //this.cinemasAnchorList.push({
+                 //  value: $(cinemasdiv).find('a')[i].getAttribute('onclick').replace('getAllCinemaData', '').replace(/[()]/g, '').split(',')[0].replace(/["']/g, ""),
+                   // name: $(cinemasdiv).find('a')[i].getAttribute('onclick').replace('getAllCinemaData', '').replace(/[()]/g, '').split(',')[1].replace(/["']/g, "")
+                //});
+               //}
   }
 
   loadWebData(){
     this.mainService.loadWebData()
     .then(data => {
       this.webdata = data;
-     alert(data);
-      this.getRequiredData();
+      this.getRequiredData(this.webdata);
       
     });
   }
-
  
-
 }
